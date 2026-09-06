712eebe7204245c428b2001efd43d85b6bd2a625
s8757520-sys <s8757520@gmail.com>
2026-09-06 10:27:05 +0300

    Update Header.jsx
    style: increase logo size to h-12 sm:h-16

--- src/appmain/components/Header.jsx
+++ src/appmain/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = ({ onMenuClick }) => {
       <img
         src={logoSrc}
         alt="Ekso"
-        className="h-10 sm:h-12 w-auto"
+        className="h-12 sm:h-16 w-auto"
       />
 
       <button
