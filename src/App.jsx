4ee20ea97e05afb1f6cb4995c8e5d3cfcca62fe8
s8757520-sys <s8757520@gmail.com>
2026-09-06 13:26:45 +0300

    Update App.jsx
    blur

--- src/App.jsx
+++ src/App.jsx
@@ -256,7 +256,7 @@ function App() {
           publicKey={masterKeyData?.wallet?.publicKey}
           lang={lang}
           onLogout={handleLogout}
-          isJustLoggedIn={step === 'main' && masterKeyData !== null}
+          isNewSession={step === 'main' && masterKeyData !== null}
         />
       ) : (
         <div className="flex items-center justify-center p-4 min-h-screen">
